d1532859492b7ca71ad013880a68c4f21a6e483d
chl8263 <chl8263@naver.com>
2019-04-16 00:11:22 +0900

    single LiveData 구현

---
+++ app/src/main/java/com/example/mvvm_livedata/utils/SingleLiveEvent.kt
@@ -0,0 +1,34 @@
+package com.example.mvvm_livedata.utils
+
+import android.nfc.Tag
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+open class SingleLiveEvent<T> : MutableLiveData<T>(){
+
+    private val mPending = AtomicBoolean(false)
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if(hasActiveObservers()) Log.w("multifle observing","옵저빙이 여러개입니다. 애는 한번만 동작합니다")
+
+        //super.observe(owner, observer)
+        super.observe(owner, Observer {
+            if(mPending.compareAndSet(true,false))  // 첫번재 파라메터 : 기댓값 , false면 동작
+                observer.onChanged(it)
+        })
+    }
+
+    @MainThread
+    override fun setValue(value: T) {
+        mPending.set(true)  //atomic boolean 을 true로 바꿔주어 이벤트를 발생시키지 않는다.
+        super.setValue(value)
+    }
+    @MainThread
+    fun call(){
+
+    }
+}
